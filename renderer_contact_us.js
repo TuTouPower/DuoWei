aa8e3be98438361a3eecfc5503bb77b0f3530c29
tutoupower <fengandelliot@gmail.com>
2023-08-06 22:09:34 +0800

    用默认浏览器打开链接

--- renderer_contact_us.js
+++ renderer_contact_us.js
@@ -1,9 +1,11 @@
-const { initI18n, i18n } = require('../scripts/i18nConfig.js');
+const { initI18n } = require('../scripts/i18nConfig.js');
+const { shell } = require('electron');
 
 const duoweiTitle       =   document.getElementById('title');
 const duoweiHeadline    =   document.getElementById('headline');
 const githubLink        =   document.querySelector('#github-link a');
 const feedbackLink      =   document.querySelector('#feedback-link a');
+const externalLinks     =   document.querySelectorAll('a[href^="http"]');
 
 window.addEventListener('DOMContentLoaded', () => {
     initI18n(function(t) {
@@ -12,4 +14,13 @@ window.addEventListener('DOMContentLoaded', () => {
         githubLink.innerText = t('contact_us_github_link');
         feedbackLink.innerText = t('contact_us_feedback_link');
     });
+
+
+    externalLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+        e.preventDefault();
+        shell.openExternal(link.href);
+        }); 
+    });
+
 });
